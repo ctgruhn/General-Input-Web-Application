5d83b02ef6e057ca182bbb89a229ac55d8d791cd
Carl Gruhn <cgruhn@bgsu.edu>
2020-09-13 14:36:16 -0400

    Create constants

---
+++ hazardapp/config.py
@@ -0,0 +1,7 @@
+AREA_LIST = [('dl1', 'Development Line 1'),
+        ('dl2', 'Development Line 2'),
+        ('caf', 'Cafeteria')]
+
+HAZARD_LIST = [('fall', 'Fall Hazard'),
+        ('shock', 'Electric Shock'),
+        ('pierce', 'Piercing Hazard')]
