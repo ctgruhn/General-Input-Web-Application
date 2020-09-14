eff6c3b6f61a7f010eba999511a75e31d0ab5f2a
Carl Gruhn <cgruhn@bgsu.edu>
2020-09-14 14:50:25 -0400

    Data structure from list of tuples to dict

--- hazardapp/config.py
+++ hazardapp/config.py
@@ -1,7 +1,11 @@
-AREA_LIST = [('dl1', 'Development Line 1'),
-        ('dl2', 'Development Line 2'),
-        ('caf', 'Cafeteria')]
+# TODO: Add to database
+AREA_LIST = {'dl1': 'Development Line 1',
+        'dl2': 'Development Line 2',
+        'off': 'Office',
+        'caf': 'Cafeteria'}
 
-HAZARD_LIST = [('fall', 'Fall Hazard'),
-        ('shock', 'Electric Shock'),
-        ('pierce', 'Piercing Hazard')]
+# TODO: Add to database
+HAZARD_LIST = {'fall': 'Fall Hazard',
+        'shock': 'Electric Shock',
+        'burn': 'Burn Hazard',
+        'pierce': 'Piercing Hazard'}
