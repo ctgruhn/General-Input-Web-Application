3d422656214db5a9a72cb86cf7b70612b0e5dd7c
Carl Gruhn <cgruhn@bgsu.edu>
2020-09-14 14:51:59 -0400

    Load user

--- hazardapp/models.py
+++ hazardapp/models.py
@@ -1,7 +1,12 @@
 from datetime import datetime
-from hazardapp import db
+from hazardapp import db, login_manager
+from flask_login import UserMixin
 
-class User(db.Model):
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     first_name = db.Column(db.String(20), nullable=False)    # may need to extend limit for longer names
@@ -19,7 +24,7 @@ class Hazards(db.Model):
     location = db.Column(db.String(20), nullable=False)
     task = db.Column(db.String(120), nullable=False)
     hazard = db.Column(db.String(20), nullable=False)
-    info = db.Column(db.Text, nullable=False)
+    details = db.Column(db.Text, nullable=False)
     date_submitted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
